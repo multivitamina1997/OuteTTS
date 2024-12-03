66df2f84be99b72d8512845e799dd910528f48b8
Joshua Lochner <admin@xenova.com>
2024-12-03 01:35:06 +0000

    Implement basic outetts.js version

---
+++ outetts.js/version/v1/model.js
@@ -0,0 +1,10 @@
+
+export class HFModel {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async generate(...args) {
+        return (await this.model.generate(...args)).tolist()[0];
+    }
+}
